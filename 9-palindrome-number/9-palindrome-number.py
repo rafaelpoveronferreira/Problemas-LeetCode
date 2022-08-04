1a2b7ee4c367afac0570f3e43a57a8e3b9aa03c6
rafaelpoveronferreira <110116277+rafaelpoveronferreira@users.noreply.github.com>
2022-08-04 15:36:02 -0300

    Update 9-palindrome-number.py

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,7 +1,17 @@
 class Solution:
-    def isPalindrome(self, x: int) -> bool:
+    # First solution: converting to string
+    def isPalindromestr(self, x: int) -> bool:
         xstr = str(x)
-        if xstr[::-1] == xstr:
-            return True
-        else:
+        return (xstr[::-1] == xstr)
+    
+    # Second solution: without converting to string
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or (x % 10 == 0 and x != 0):
             return False
+        
+        reversedx = 0
+        while (x > reversedx):
+            reversedx = (x % 10) + reversedx*10
+            x = int(x/10)
+        
+        return x == reversedx or (x == int(reversedx/10))
