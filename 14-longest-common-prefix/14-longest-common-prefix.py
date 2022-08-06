05dfba195381db53443cd6432878e20bc0a042f5
rafaelpoveronferreira <110116277+rafaelpoveronferreira@users.noreply.github.com>
2022-08-06 15:46:58 -0300

    Update 14-longest-common-prefix.py

--- 14-longest-common-prefix/14-longest-common-prefix.py
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -1,22 +1,16 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        smallest = strs[0]
         prefix = ''
         c = 0
 
-        # Find the smallest number in strs list
-        for i, _ in enumerate(strs):
-            if len(smallest) > len(strs[i]):
-                smallest = strs[i]
-
-        # A double for iteration that looks for each character in each string of strs list.
-        for char in range(len(smallest)):
-            c=0
+        # A simple double for iteration that compares each character in each string of strs list.
+        for char in range(len(min(strs))):
             for i, _ in enumerate(strs):
-                if smallest[char] == strs[i][char]:
+                if min(strs)[char] == strs[i][char]:
                     c += 1
+            # If the all the character was present in every string, add it to prefix string
             if c == len(strs):
-                prefix += smallest[char]
+                prefix += min(strs)[char]
                 c = 0
             else:
                 break
